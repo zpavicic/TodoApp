628c285286ce86e919ca45552c123f3967cf02ca
zeljko <contactozeljkopavicic@gmail.com>
2023-12-05 17:29:11 -0300

    V 1.0

---
+++ app/src/main/java/com/example/todoapp/OnDialogCloseListener.java
@@ -0,0 +1,9 @@
+package com.example.todoapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+
+    void onDialogClose(DialogInterface dialogInterface);
+
+}
---
+++ app/src/main/java/com/example/todoapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.todoapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 4000);
+    }
+}
